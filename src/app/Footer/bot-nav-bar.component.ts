7e1be8ccdf820df510202ddede45c43afbe603d2
kevin7zheng <kz0010@mix.wvu.edu>
2021-09-07 18:50:40 -0400

    Add 2 more component

---
+++ src/app/Footer/bot-nav-bar.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component ({  selector: 'app-bot-nav-bar',
+templateUrl: 'bot-nav-bar.component.html',
+styleUrls: ['./bot-nav-bar.component.css']
+})
+export class BotNavBarComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BotNavBarComponent } from './Footer/bot-nav-bar.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { PageComponent } from './pagination/page.component';
+import { SideNavBarComponent } from './sidebar/side-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent, BotNavBarComponent, SideNavBarComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule
